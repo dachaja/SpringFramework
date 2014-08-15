c87e98fc6c8c8361aa6b25495e73168d7b5e1bfd
dachaja <pettitbusiness@gmail.com>
2014-08-15 00:12:58 -0700

    sqltest

---
+++ STUDY/day02/workspace/SQLTest/bin/sql_test.sql
@@ -0,0 +1,23 @@
+create table users(
+	id varchar2(8) primary key,
+	password varchar2(8),
+	name varchar2(20),
+	role varchar2(5)
+);
+
+insert into users values('test','test','AJ','Admin');
+insert into users values('aaa','aaa','홍길동','User');
+
+create table board(
+	seq number(5) primary key,
+	title varchar2(200),
+	writer varchar2(20),
+	content varchar2(2000),
+	regdate date default sysdate,
+	cnt number(5) default 0
+);
+
+insert into board (seq, title, writer, content) values(1,'가입인사','홍길동','잘 부탁드립니다.');
+
+select * from users;
+select * from board;
---
+++ STUDY/day02/workspace/TestWeb/target/TestWeb/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello TestWeb!</h2>
+</body>
+</html>
